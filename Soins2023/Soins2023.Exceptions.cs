a6a23c6f54bac84ea9cb3ff7b82beb0e601ab010
Egor GUTUTUI <egor.gututui@lyc-bonaparte.fr>
2023-09-18 11:32:07 +0200

    creation soinsException + logs

---
+++ Soins2023/Soins2023.Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soins2023
+{
+    class SoinsException: Exception
+    {
+        string fileName = ConfigurationManager.AppSettings["LogFileName"];
+
+        public SoinsException(string message)
+            : base("Erreur de : " + System.Environment.UserName + " le " + DateTime.Now + "\n" + message)
+        {
+            StreamWriter sw = new StreamWriter(fileName, true);
+            sw.WriteLine("Erreur de : " + System.Environment.UserName + " le " + DateTime.Now);
+            sw.WriteLine(message);
+            sw.Close();
+        }
+    }
+}
